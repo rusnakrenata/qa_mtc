1827c5f14ac3c51390c91be7ab8025b11c63e078
rusnakrenata <rusnak.renata@gmail.com>
2025-04-24 17:24:47 +0200

    compute_congestion.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from db_tables import *
 from city_map import store_in_db_city
 from generate_cars import store_in_db_cars
 from generate_routes import store_in_db_car_routes
+from comupte_congestion import store_in_db_congestion_scores
 from sqlalchemy.orm import sessionmaker
 
 # ---------- CONFIGURATION ----------
@@ -49,6 +50,9 @@ else:
 CARS, ITERATION_ID = store_in_db_cars(G, N_CARS, MAX_LENGTH, MIN_LENGTH, RUN_ID)
 
 # Store CAR routes in db for the specific iteration ID
-store_in_db_car_routes(CARS, API_KEY, K_ALTERNATIVES, RUN_ID, ITERATION_ID)
+CAR_ROUTES = store_in_db_car_routes(CARS, API_KEY, K_ALTERNATIVES, RUN_ID, ITERATION_ID)
+
+# Calculate and store Congestion Scores
+CONGESTION_SCORES = store_in_db_congestion_scores(CAR_ROUTES, RUN_ID, ITERATION_ID)
 
 session.close()
