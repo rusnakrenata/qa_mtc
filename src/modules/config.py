b177855668f46d50b3a042b43b667f77a1d434ec
Renáta Rusnáková <rusnak.renata@gmail.com>
2025-10-31 10:04:39 +0100

    analyzes

--- src/modules/config.py
+++ src/modules/config.py
@@ -4,10 +4,10 @@
 #CITY_NAME = "Bratislava, Slovakia"
 #CENTER_COORDS = None#(48.7208, 21.2575)
 #RADIUS_KM = None
-CITY_NAME = "Prague, Czech Republic"
-CENTER_COORDS =(50.0818,14.4265) #Mala strana
-RADIUS_KM = 1
-N_VEHICLES = 15000
+CITY_NAME = "Barcelona, Spain"
+CENTER_COORDS = (41.392937, 2.145062)#(41.384500, 2.129500) # Barcelona
+RADIUS_KM = 3.2
+N_VEHICLES = 10000
 K_ALTERNATIVES = 3
 MIN_LENGTH = 500
 MAX_LENGTH = 4000
@@ -16,14 +16,14 @@ TIME_WINDOW = 300
 DISTANCE_FACTOR = 4.0  # Factor to adjust distance in congestion calculations
 CLUSTER_RESOLUTION = 4.0  # Resolution for clustering in connectivity analysis
 
-ATTRACTION_POINT = (50.083704,14.432587) # Hlavak
-D_ALTERNATIVES = 4
+ATTRACTION_POINT = None#(41.380898, 2.122820)#(50.083704,14.432587) # Hlavak
+D_ALTERNATIVES = None#4
 
 # --- QUBO/QA Parameters ---
 COMP_TYPE = "hybrid"             # 'sa', 'hybrid', or 'qpu'
 ROUTE_METHOD = "duration"       # or "distance"
 MIN_CLUSTER_SIZE = 500 #400,1000,1100
-MAX_CLUSTERS = 30
+MAX_CLUSTERS = 200
 FULL = False
 
 
